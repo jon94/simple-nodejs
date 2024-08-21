f97562d3c2f90dd1df6a4345a4af2c7dfd45f4de
wipawan <mew.amornchairattanakul@datadoghq.com>
2024-08-21 12:55:52 +0800

    test

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
 // Define a GET endpoint at /api
-app.get('/api', (req, res) => {
+app.get("/api", (req, res) => {
   const apiResponse = {
-    message: 'Hello, this is a simple API!',
+    message: "Hello, this is a simple API!",
     date: new Date(),
   };
 
@@ -14,15 +14,30 @@ app.get('/api', (req, res) => {
 });
 
 // Define a GET endpoint at /getErrorRequest to simulate an application error
-app.get('/getErrorRequest', (req, res) => {
+app.get("/getErrorRequest", (req, res) => {
   // Simulate an error by throwing an exception
-  throw new Error('This is a simulated application error');
+  throw new Error("This is a simulated application error");
 });
 
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ error: 'Internal Server Error' });
+  res.status(500).json({ error: "Internal Server Error" });
+});
+
+app.param("id", function (req, res, next, id) {
+  console.log("CALLED ONLY ONCE");
+  next();
+});
+
+app.get("/user/:id", function (req, res, next) {
+  console.log("although this matches");
+  next();
+});
+
+app.get("/user/:id", function (req, res) {
+  console.log("and this matches too");
+  res.end();
 });
 
 // Start the server
